b5253ec27e641c8b4458fcdc4b49badb927184e2
Abhigyan1112 <abhigyansharma.hhh@gmail.com>
2025-06-09 19:38:29 +0530

    9 June

--- Image_Scanning/scanner.py
+++ Image_Scanning/scanner.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import numpy as np
 from mongoengine import connect, Document, StringField, IntField
 from flask import redirect, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_toastr import Toastr
 import cv2
-import os
 import dlib
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+db=SQLAlchemy(app)
+toastr=Toastr(app)
+
+class Image(db.Model):
+    booking_id=db.Column(db.String(200),nullable=False)
+    image_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    image=db.Column(db.LargeBinary,nullable=False)
+
+    def __repr__(self) -> str:
+        return f'{self.booking_id} - {self.image_id}'
 
 # #Connecting to the MongoDB database 'trial_database' with connection string
 # connect(
@@ -13,81 +26,70 @@ app=Flask(__name__)
 #     host='mongodb+srv://Abhigyan:%40Saransh16@user-images.48yzbzo.mongodb.net/'
 # )
 
-# class Entry(Document):
-#     name=StringField(required=True)
-#     age= IntField(required=True)
-#     email=StringField(required=True)
+ # class Image(Document):
+ #     bookingID=StringField(required=True)
+ #     imageID=IntField(required=True)
+ #     image=FileField(required=True)
 
 @app.route("/")
 def main():
-    return "Welcome to the home page"
+    return render_template("index.html")
 
-@app.route("/image-processing",methods=['GET'])
+@app.route("/image-processing",methods=['POST'])
 def image_processing():
-    path='static/Images'
-    opath='static/marked_Images'
-    result=[]
+    booking_id=request.form['bookingID']
+    image=request.files['image'].read()
+    try:
+        if image is None:
+            return jsonify({'error': 'Failed to load image with OpenCV. Check format.'}), 400
 
-    for image in os.listdir(path):
-        image_path=os.path.join(path,image)
-        try:
-            detector = dlib.get_frontal_face_detector()
-            predictor = dlib.shape_predictor("static/shape_predictor_68_face_landmarks.dat")
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor("static/shape_predictor_68_face_landmarks.dat")
 
-            img=cv2.imread(image_path)
+        img=cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
+        rgb=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        face=detector(rgb)
 
-            if img is None:
-                return jsonify({'error': 'Failed to load image with OpenCV. Check format or path.'}), 400
-
-            
-            rgb=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            faces=detector(rgb)
-
-            if len(faces) == 0:
-                result.append({
-                    'file_name': image,
-                    'status': 'Cannot detect any faces in the image'
-                })
-                continue
-
-            for face in faces:
-                landmarks = predictor(rgb, face)
-                points = [(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)]
-
-                for (x,y) in points:
-                    cv2.circle(img, (x,y), 2, (255,0,0), -1)
-
-                nose = (landmarks.part(30).x, landmarks.part(30).y)
-                left_cheek = (landmarks.part(1).x, landmarks.part(1).y)
-                right_cheek = (landmarks.part(15).x, landmarks.part(15).y)
-
-                left_dist = abs(nose[0] - left_cheek[0])
-                right_dist = abs(right_cheek[0] - nose[0])
-                symmetry_ratio = left_dist / right_dist
-
-
-            output_path = os.path.join(opath,('marked_'+image))
-            cv2.imwrite(output_path, img)
-
-            if symmetry_ratio > 2:
-                result.append({
-                    'file_name': image,
-                    'status': 'The image is not frontal',
-                    'smmetry_ratio': symmetry_ratio
-                })
-                continue
-
-            result.append({
+        if len(face) == 0:
+            return jsonify({
                 'file_name': image,
-                'output_image': output_path,
-                'Symmetry_ratio' : symmetry_ratio
-            })
-        
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
+                'status': 'Cannot detect any faces in the image'
+            }), 400
 
-    return jsonify({'results': result}), 200
+        if len(face) > 1:
+            return jsonify({
+                'file_name': image,
+                'status': 'Multiple faces in the image'
+            }), 400
+
+        landmarks = predictor(rgb, face[0])
+        points = [(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)]
+
+        for (x,y) in points:
+            cv2.circle(img, (x,y), 2, (255,0,0), -1)
+
+        nose = (landmarks.part(30).x, landmarks.part(30).y)
+        left_cheek = (landmarks.part(1).x, landmarks.part(1).y)
+        right_cheek = (landmarks.part(15).x, landmarks.part(15).y)
+
+        left_dist = abs(nose[0] - left_cheek[0])
+        right_dist = abs(right_cheek[0] - nose[0])
+        symmetry_ratio = left_dist / right_dist
+
+        if symmetry_ratio > 1.5:
+            return jsonify({
+                'file_name': image,
+                'status': 'The image is not frontal',
+                'symmetry_ratio': symmetry_ratio
+            }), 400
+
+        img=Image(booking_id=booking_id,image=image)
+        db.session.add(img)
+        db.session.commit()
+        return render_template('index.html')
+
+    except Exception as e:
+        return jsonify({'error Exception': str(e)}), 500
 
 # @app.route("/entry",methods=['GET','POST'])
 # def entry():
